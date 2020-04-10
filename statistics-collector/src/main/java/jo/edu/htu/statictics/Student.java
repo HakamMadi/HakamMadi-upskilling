be473c3bd777b713c017f77705303c2590a79f49
HakamMadi <HakamMadi@github.com>
2020-04-10 18:36:32 +0300

    Student and studentsCollector (bad practice)

--- statistics-collector/src/main/java/jo/edu/htu/statictics/Student.java
+++ statistics-collector/src/main/java/jo/edu/htu/statictics/Student.java
@@ -3,67 +3,80 @@ package jo.edu.htu.statictics;
 import java.time.Year;
 
 public class Student {
-
-    public static Builder builder() {
-        return new Builder();
-    }
+    String id;
+    String name;
+    Year graduationYear;
+    String university;
+    String major;
+    Gender gender;
 
     private Student() {
 
     }
 
+    public static Builder builder() {
+        return new Builder();
+    }
+
     public String getId() {
-        return null;
+        return id;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Year getGraduationYear() {
-        return null;
+        return graduationYear;
     }
 
     public String getUniversity() {
-        return null;
+        return university;
     }
 
     public String getMajor() {
-        return null;
+        return major;
     }
 
     public Gender getGender() {
-        return null;
+        return gender;
     }
 
     public static class Builder {
+        Student student = new Student();
+
         public Builder setId(String id) {
+            student.id = id;
             return this;
         }
 
         public Builder setName(String name) {
+            student.name = name;
             return this;
         }
 
         public Builder setGraduationYear(Year graduationYear) {
+            student.graduationYear = graduationYear;
             return this;
         }
 
         public Builder setUniversity(String university) {
+            student.university = university;
             return this;
         }
 
-
         public Builder setMajor(String major) {
+            student.major = major;
             return this;
         }
 
         public Builder setGender(Gender gender) {
+            student.gender = gender;
             return this;
         }
 
         public Student build() {
-            return null;
+            return student;
         }
     }
 }
--- statistics-collector/src/main/java/jo/edu/htu/statictics/StudentsCollector.java
+++ statistics-collector/src/main/java/jo/edu/htu/statictics/StudentsCollector.java
@@ -1,8 +1,180 @@
 package jo.edu.htu.statictics;
 
+import java.time.Year;
+import java.util.Arrays;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
 public class StudentsCollector implements StatisticsCollector<Student> {
     @Override
     public Iterable<Statistic> collect(Iterable<Student> cases) {
-        return null;
+        Statistic majorCISStatistic = getCISMajor(cases);
+        Statistic graduationYearAfter = getGraduationYearAfter(cases);
+        Statistic majorCSStatistic = getCSMajor(cases);
+        Statistic graduationYearBefore = getGraduationYearBefore(cases);
+        Statistic maleStudents = getMaleStudents(cases);
+        Statistic femaleStudents = getFemaleStudents(cases);
+
+
+        return Arrays.asList
+                (majorCISStatistic, graduationYearAfter, majorCSStatistic, graduationYearBefore, maleStudents, femaleStudents);
     }
+
+    private Statistic getCISMajor(Iterable<Student> cases) {
+        return new Statistic() {
+            @Override
+            public String name() {
+                return "CIS major";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                cases.forEach(student -> {
+                    if (student.getMajor().equals("CIS")) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+    private Statistic getCSMajor(Iterable<Student> cases) {
+        return new Statistic() {
+            AtomicReference<String> major = new AtomicReference<>("");
+
+            @Override
+            public String name() {
+                return "Computer Science major";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                cases.forEach(student -> {
+                    if (student.getMajor().equals("Computer Science")) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+    private Statistic getGraduationYearAfter(Iterable<Student> cases) {
+        return new Statistic() {
+            @Override
+            public String name() {
+                return "graduation year > 2016";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                Year year = Year.of(2016);
+                cases.forEach(student -> {
+                    if (student.getGraduationYear().isAfter(year)) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+    private Statistic getGraduationYearBefore(Iterable<Student> cases) {
+        return new Statistic() {
+            @Override
+            public String name() {
+                return "graduation year <= 2016";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                Year year = Year.of(2016);
+                cases.forEach(student -> {
+                    if (student.getGraduationYear().isBefore(year) || student.getGraduationYear().equals(year)) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+    private Statistic getMaleStudents(Iterable<Student> cases) {
+        return new Statistic() {
+            @Override
+            public String name() {
+                return "Males";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                Year year = Year.of(2016);
+                cases.forEach(student -> {
+                    if (student.getGender().equals(Gender.MALE)) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+    private Statistic getFemaleStudents(Iterable<Student> cases) {
+        return new Statistic() {
+            @Override
+            public String name() {
+                return "Females";
+            }
+
+            @Override
+            public Integer matchedCases() {
+                AtomicInteger matchedCases = new AtomicInteger();
+                Year year = Year.of(2016);
+                cases.forEach(student -> {
+                    if (student.getGender().equals(Gender.FEMALE)) {
+                        matchedCases.getAndIncrement();
+                    }
+                });
+                return matchedCases.get();
+            }
+
+            @Override
+            public int compareTo(Statistic o) {
+                return 0;
+            }
+        };
+    }
+
+
 }
